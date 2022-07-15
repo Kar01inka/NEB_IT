7df5d7b189cbb3c1e9b46909926c06eda6b6a3b7
Karolina Pilarz <101668855+Kar01inka@users.noreply.github.com>
2022-07-15 19:44:37 +0200

    Update help.sh

--- help.sh
+++ help.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ $1=="--help" ] ;
+if [ $1=="--help" ]  || [ $1=="-h" ] ;
 then
 cat << EOF
 --date | -d: Wyświetlenie daty
 --logs | -l: Utworzenie 100 plików o nazwie logsx.txt, gdzie x to numer od 1 do 100
---logs x | -l x; Utworzenie x plików o nazwie logsx.txt
+--logs x | -l x: Utworzenie x plików o nazwie logsx.txt
+--help | -h: Wyswietlenie pomocy
 EOF
 fi
